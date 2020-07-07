b351acf25682f5fcd5a7bc9814c65c4eb81520e1
Florent gravin <florent.gravin@camptocamp.com>
2020-07-07 15:06:12 +0200

    Add trainee

--- src/main.js
+++ src/main.js
@@ -13,7 +13,8 @@ import Style from 'ol/style/Style'
 import Stroke from 'ol/style/Stroke'
 
 const FEATURE_COUNT = 50
-const INTERVAL_TIME = 5000
+const INTERVAL_TIME = 2000
+const POINT_RADIUS = 7
 
 let startAnimation
 
@@ -30,7 +31,7 @@ const map = new Map({
     center: ADL_CENTER,
     zoom: ADL_ZOOM
   })
-});
+})
 
 const agentPositions = new Position(FEATURE_COUNT)
 agentPositions.init(ADL_EXTENT)
@@ -40,19 +41,19 @@ const agentsLayer = new VectorLayer({
     features: agentPositions.getPositions(),
   }),
   disableHitDetection: true,
-/*
-  style: new Style({
-    image: new Circle({
-      stroke: new Stroke({
-        color: 'rgba(0,0,255,1)',
-        width: 1
-      }),
-      fill: new Fill({
-        color: 'rgba(255,255,255,0.1)'
+  /*
+    style: new Style({
+      image: new Circle({
+        stroke: new Stroke({
+          color: 'rgba(0,0,255,1)',
+          width: 1
+        }),
+        fill: new Fill({
+          color: 'rgba(255,255,255,0.1)'
+        })
       })
     })
-  })
-*/
+  */
 })
 
 
@@ -62,22 +63,37 @@ const triangle = new RegularShape({
   points: 3,
   radius: 14,
 })
-const image = triangle.getImage();
-const ctx = image.getContext("2d");
-const c2 = document.createElement('canvas');
-c2.width = c2.height = image.width;
-c2.getContext("2d").drawImage(image, 0,0);
-ctx.clearRect(0,0,image.width,image.height);
-ctx.drawImage(c2, 0,0, image.width, image.height, 6, 0, image.width-12, image.height);
+const image = triangle.getImage()
+const ctx = image.getContext('2d')
+const c2 = document.createElement('canvas')
+c2.width = c2.height = image.width
+c2.getContext('2d').drawImage(image, 0, 0)
+ctx.clearRect(0, 0, image.width, image.height)
+ctx.drawImage(c2, 0, 0, image.width, image.height, 6, 0, image.width - 12, image.height)
 
 
-const agentsLayerStyle = new Style({
-  // image: triangle
+const agentsLayerStylePoint = new Style({
   image: new Circle({
     fill: new Fill({color: 'red'}),
-    radius: 5
+    radius: POINT_RADIUS
   })
+})
+const traineeStyle = new Style({
+  image: new Circle({
+    fill: new Fill({color: 'rgba(100,100,100,.8)'}),
+    radius: POINT_RADIUS
+  })
+})
 
+const traineeStyle2 = new Style({
+  image: new Circle({
+    fill: new Fill({color: 'rgba(100,100,100,.7)'}),
+    radius: POINT_RADIUS-2
+  })
+})
+
+const agentsLayerStyleTriangle = new Style({
+  image: triangle
 })
 
 map.addLayer(agentsLayer)
@@ -89,7 +105,7 @@ agentsLayer.on('postrender', (event) => {
   const layer = event.target
 
   layer.getSource().forEachFeature(feature => {
-    if(startAnimation != undefined) {
+    if (startAnimation != undefined) {
       const timeRatio = (frameState.time - startAnimation) / INTERVAL_TIME
       const p1 = feature.getGeometry().getCoordinates()
       const p0 = feature.get('lastCoord')
@@ -102,14 +118,23 @@ agentsLayer.on('postrender', (event) => {
         dY * timeRatio + p0[1],
       ]
 
-      const rotation = Math.PI/2 + Math.atan2(p0[1] - p1[1], p0[0] - p1[0])
-      agentsLayerStyle.getImage().setRotation( -rotation)
+      //const speed = Math.sqrt(dX*dX+dY*dY) / (INTERVAL_TIME / 3600)
+      const steps = [5, 10, 15, 25, 40, 60]
+      vectorContext.setStyle(traineeStyle)
+      steps.forEach(ratio => {
+        if(ratio >= 15) {
+          vectorContext.setStyle(traineeStyle2)
+        }
+        vectorContext.drawGeometry(new Point([newPos[0] - (dX * ratio/100), newPos[1] - (dY * ratio/100)]))
+        })
+      const rotation = Math.PI / 2 + Math.atan2(p0[1] - p1[1], p0[0] - p1[0])
+      agentsLayerStylePoint.getImage().setRotation(-rotation)
 
-      vectorContext.setStyle(agentsLayerStyle)
+      vectorContext.setStyle(agentsLayerStylePoint)
       vectorContext.drawGeometry(new Point(newPos))
     }
   })
-  map.render();
+  map.render()
 })
 
 setInterval(() => {
